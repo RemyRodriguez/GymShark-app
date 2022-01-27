2cdcbfefa34137cd5ba013ed121a618890b19a6c
Remy <rer0020@mix.wvu.edu>
2022-01-26 19:28:58 -0500

    Lab1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SecondTopNavBarComponent } from './navigation/second.topnavbar.component';
+import { TopNavbarComponent } from './navigation/topnavbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    TopNavbarComponent,
+    SecondTopNavBarComponent
+],
   imports: [
     BrowserModule
   ],
