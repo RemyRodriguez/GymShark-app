7f83cc87d6c6f9e063b791668d2d8cda7c666ebb
Remy <rer0020@mix.wvu.edu>
2022-02-03 01:12:27 -0500

    testing again

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { FooterComponent } from './Footer/footer.component';
 
 import { TopNavbarComponent } from './Header/topnavbar.component';
+import { CategoryCardComponent } from './Store/catagory.card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopNavbarComponent,
-    FooterComponent
+    FooterComponent,
+    CategoryCardComponent
 ],
   imports: [
     BrowserModule
